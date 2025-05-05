4979d9fbfaba618256583588b630f3be67e504d5
Nelson-Lamounier <lamounierincolor@gmail.com>
2025-05-05 09:13:38 +0100

    navbarData.json update

--- src/component/Navbar/Navbar.component.tsx
+++ src/component/Navbar/Navbar.component.tsx
@@ -1,13 +1,20 @@
+/** @format */
+
 import { useEffect, useState, FC } from "react";
 import { useLocation, Link } from "react-router-dom";
 import { useMediaQuery } from "react-responsive";
-import { NavbarContainer, Logo, NavList, NavBar,  MobileMenuIcon, 
-  MobileNavMenu  } from "./navbar.styled";
+import {
+  NavbarContainer,
+  Logo,
+  NavList,
+  NavBar,
+  MobileMenuIcon,
+  MobileNavMenu,
+} from "./navbar.styled";
 
 //Use the Intersection Observer API to detect when a section is in the viewport and dynamically update the active state of the corresponding nav link.
 
 import navbarData from "../../data/navbarData.json";
-
 
 const Navbar: FC = () => {
   const [isScrolled, setIsScrolled] = useState(false);
@@ -17,23 +24,22 @@ const Navbar: FC = () => {
   const { logo, links } = navbarData;
   const isAbove640px = useMediaQuery({ minWidth: 640 });
 
-    // Check if the user is on the project page
-    const isProjectPage = location.pathname.startsWith("/project/");
+  // Check if the user is on the project page
+  const isProjectPage = location.pathname.startsWith("/project/");
 
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY === 0) {
+        setIsScrolled(false); // Reset `isScrolled` when at the top
+        setActiveSection("home");
+      } else if (window.scrollY > 399) {
+        setIsScrolled(true); // Set `isScrolled` when scrolling down
+      }
+    };
 
-    useEffect(() => {
-      const onScroll = () => {
-        if (window.scrollY === 0) {
-          setIsScrolled(false); // Reset `isScrolled` when at the top
-          setActiveSection("home");
-        } else if (window.scrollY > 399) {
-          setIsScrolled(true); // Set `isScrolled` when scrolling down
-        }
-      };
-    
-      window.addEventListener("scroll", onScroll);
-      return () => window.removeEventListener("scroll", onScroll);
-    }, []);
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
 
   useEffect(() => {
     const observerOptions = {
@@ -48,102 +54,112 @@ const Navbar: FC = () => {
           currentActiveSection = entry.target.id; // Update active section to the currently intersecting section
         }
       });
-  
+
       if (currentActiveSection) {
         setActiveSection(currentActiveSection); // Set the active section if one is intersecting
       } else if (window.scrollY === 0) {
         setActiveSection(""); // If at the top of the page, set no active section
       }
-
     }, observerOptions);
-  
+
     const sections = document.querySelectorAll("section");
     sections.forEach((section) => observer.observe(section));
-  
+
     return () => observer.disconnect(); // Clean up on component unmount
   }, []);
 
-    // Toggle the mobile menu
-    const toggleMenu = () => {
-      setIsMenuOpen((prev) => !prev);
-    };
-  
-
-    return (
-      <NavbarContainer>
-        {/* Conditionally Render Navbar */}
-        {isProjectPage ? (
-          <NavBar className={`navbar ${
-              isScrolled ? "navbar-colored" : "navbar-transparent"
-            }`}>
-            {/* Minimal Navbar for Project Page */}
-            <Logo>
-              <Link to="/">
-                <img src={logo.src} alt={logo.alt} />
-              </Link>
-            </Logo>
-            <NavList>
-              <Link to="/" className="nav-link">
-                ← Back to Home
-              </Link>
-            </NavList>
-          </NavBar>
-        ) : (
-          <NavBar
-            className={`navbar ${
-              isScrolled ? "navbar-colored" : "navbar-transparent"
-            }`}
-          >
-            {/* Full Navbar for Other Pages */}
-            <Logo>
-              <a href="#">
-                <img src={logo.src} alt={logo.alt} />
-              </a>
-            </Logo>
-            <NavList>
-              {links.map((link, index) => (
-                (isAbove640px || link.label !== "Resume") && (
-                <a
-                  key={index}
-                  href={link.href}
-                  className={`nav-link ${
-                    activeSection === link.href.slice(1) ? "active" : ""
-                  }`}
-                >
-                  {link.label}
-                </a>
-                    )
-              ))}
-            </NavList>
-            <MobileMenuIcon onClick={toggleMenu}>
-              {isMenuOpen ? (
-                <i className="fas fa-times"></i>
-              ) : (
-                <i className="fas fa-bars"></i>
-              )}
-            </MobileMenuIcon>
-          </NavBar>
-        )}
-  
-        {/* Mobile Navigation Menu */}
-        {!isProjectPage && (
-          <MobileNavMenu isOpen={isMenuOpen}>
-            {links.map((link, index) => (
-              <a
-                key={index}
-                href={link.href}
-                className={`nav-link ${
-                  activeSection === link.href.slice(1) ? "active" : ""
-                }`}
-                onClick={() => setIsMenuOpen(false)}
-              >
-                {link.label}
-              </a>
-            ))}
-          </MobileNavMenu>
-        )}
-      </NavbarContainer>
-    );
+  // Toggle the mobile menu
+  const toggleMenu = () => {
+    setIsMenuOpen((prev) => !prev);
   };
-  
-  export default Navbar;
+
+  return (
+    <NavbarContainer>
+      {/* Conditionally Render Navbar */}
+      {isProjectPage ? (
+        <NavBar
+          className={`navbar ${
+            isScrolled ? "navbar-colored" : "navbar-transparent"
+          }`}
+        >
+          {/* Minimal Navbar for Project Page */}
+          <Logo>
+            <Link to="/">
+              <img
+                src={logo.src}
+                alt={logo.alt}
+              />
+            </Link>
+          </Logo>
+          <NavList>
+            <Link
+              to="/"
+              className="nav-link"
+            >
+              ← Back to Home
+            </Link>
+          </NavList>
+        </NavBar>
+      ) : (
+        <NavBar
+          className={`navbar ${
+            isScrolled ? "navbar-colored" : "navbar-transparent"
+          }`}
+        >
+          {/* Full Navbar for Other Pages */}
+          <Logo>
+            <a href="#">
+              <img
+                src={logo.src}
+                alt={logo.alt}
+              />
+            </a>
+          </Logo>
+          <NavList>
+            {links.map(
+              (link, index) =>
+                (isAbove640px || link.label !== "Resume") && (
+                  <a
+                    key={index}
+                    href={link.href}
+                    className={`nav-link ${
+                      activeSection === link.href.slice(1) ? "active" : ""
+                    }`}
+                  >
+                    {link.label}
+                  </a>
+                )
+            )}
+          </NavList>
+          <MobileMenuIcon onClick={toggleMenu}>
+            {isMenuOpen ? (
+              <i className="fas fa-times"></i>
+            ) : (
+              <i className="fas fa-bars"></i>
+            )}
+          </MobileMenuIcon>
+        </NavBar>
+      )}
+
+      {/* Mobile Navigation Menu */}
+      {!isProjectPage && (
+        <MobileNavMenu isOpen={isMenuOpen}>
+          {links.map((link, index) => (
+            <a
+              key={index}
+              href={link.href}
+              className={`nav-link ${
+                activeSection === link.href.slice(1) ? "active" : ""
+              }`}
+              onClick={() => setIsMenuOpen(false)}
+            >
+              {link.label}
+            </a>
+          ))}
+        </MobileNavMenu>
+      )}
+    </NavbarContainer>
+  );
+};
+
+export default Navbar;
