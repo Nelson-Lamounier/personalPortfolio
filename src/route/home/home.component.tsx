b48f767ca927c578d68e301955d3931260da1857
Nelson-Lamounier <lamounierincolor@gmail.com>
2025-05-05 10:36:09 +0100

    home component update with section id's

--- src/route/home/home.component.tsx
+++ src/route/home/home.component.tsx
@@ -1,3 +1,5 @@
+/** @format */
+
 import Header from "../../component/header/header.component";
 import { useMediaQuery } from "react-responsive";
 
@@ -5,8 +7,7 @@ import AboutMe from "../../component/about-me/about-me.component";
 import Projects from "../../component/projects/project.component";
 import ContactForm from "../../component/contact-form/contact-form.component";
 
-
-import { HomeContainer,  } from "./home.style";
+import { HomeContainer } from "./home.style";
 
 import Resume from "../../component/resume/resume.component";
 import ContactInfo from "../../component/contact/info/contact-info.component";
@@ -17,12 +18,24 @@ const Home = () => {
   const isAbove640px = useMediaQuery({ minWidth: 640 });
   return (
     <HomeContainer>
-      <Header />
-      <AboutMe />
-      <Projects />
-      {isAbove640px && <Resume />}
-      <ContactForm />
-      <ContactInfo />
+      <section id="home">
+        <Header />
+      </section>
+      <section id="about-me">
+        <AboutMe />
+      </section>
+      <section id="portfolio">
+        <Projects />
+      </section>
+      {isAbove640px && (
+        <section id="resume">
+          <Resume />
+        </section>
+      )}
+      <section id="contact">
+        <ContactForm />
+        <ContactInfo />
+      </section>
     </HomeContainer>
   );
 };
